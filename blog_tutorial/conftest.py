b87390b559a012ca21db625c8d8eb677831a3063
tierraskyllc <support@tierrasky.com>
2020-10-29 12:22:36 -0400

    initial commit

---
+++ blog_tutorial/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from blog_tutorial.users.models import User
+from blog_tutorial.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
